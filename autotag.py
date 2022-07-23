0f9a9ecf00f4a0cd1e207337cf42d997ec08bfd3
Ronald Chang <rchangmh@gmail.com>
2022-07-23 17:19:20 -0400

    updated for multi collections

--- autotag.py
+++ autotag.py
@@ -14,7 +14,7 @@ def call_raindrop_api(api_path, raindrop_id, error_response, method='GET', json=
         else:
             response = requests.get(f'https://api.raindrop.io/rest/v1/{api_path}/{raindrop_id}', headers=headers)
         if response.status_code == 429:
-            retry_time = int(response.headers['Retry-After'])
+            retry_time = int(response.headers['Retry-After']) + 5
             print(f'\nToo many requests, retrying in {retry_time}...\n')
             time.sleep(retry_time)
             return call_raindrop_api(method, api_path, raindrop_id, error_response)
@@ -52,27 +52,32 @@ def if_need_tags(raindrop_id, less_than=2):
     ).get('item', {}).get('tags', [])
     return len(tags) < less_than
 
-def main():
-    collection = Collection.get(api, '26107681')
-    # collection = CollectionRef.Unsorted
-    page = 0
-    # items = Raindrop.search(api, collection=collection, page=page)
-    while (items := Raindrop.search(api, collection=collection, page=page)):
-        print(f'\nPAGE {page}:\n')
-        for index, item in enumerate(items):
-            try:
-                need_tags = if_need_tags(item.id)
-                if need_tags:
-                    try:
-                        print(f'+ {item.title}')
-                    except Exception as error:
-                        print(f' + ___ {error}')
-                    suggested_tags = get_suggested_tags(item.id)
-                    if suggested_tags:
-                        print(f'    {suggested_tags}')
-                        tag_raindrop(item.id, suggested_tags)
-            except Exception as error:
-                print(f'Issues with https://app.raindrop.io/my/0/item/{item.id}, {error}, skipping...')
-        page += 1
+def main(collection_ids=[], start_page=0):
+    for collection_id in collection_ids:
+        if collection_id == 'unsorted':
+            collection = CollectionRef.Unsorted
+        else:
+            collection = Collection.get(api, collection_id)
+        page = start_page
+        while (items := Raindrop.search(api, collection=collection, page=page)):
+            print(f'\nID: {collection_id}: PAGE {page}:\n')
+            for index, item in enumerate(items):
+                try:
+                    need_tags = if_need_tags(item.id)
+                    if need_tags:
+                        try:
+                            print(f'+ {item.title}')
+                        except Exception as error:
+                            print(f' + ___ {error}')
+                        suggested_tags = get_suggested_tags(item.id)
+                        if suggested_tags:
+                            print(f'    {suggested_tags}')
+                            tag_raindrop(item.id, suggested_tags)
+                except Exception as error:
+                    print(f'Issues with https://app.raindrop.io/my/0/item/{item.id}, {error}, skipping...')
+            page += 1
 
-main()
+main(
+    config.collection_ids, 
+    config.start_page
+)
